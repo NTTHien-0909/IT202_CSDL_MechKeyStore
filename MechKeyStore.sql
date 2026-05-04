bec4c9fb0cd5fda862567878898fa1e69d753e4c
Nguyễn Thị Thu Hiền <nguyentth0909@gmail.com>
2026-05-04 09:14:57 +0700

    commit

--- MechKeyStore.sql
+++ MechKeyStore.sql
@@ -12,9 +12,9 @@ create table product (
 create table customer (
 	customer_id int primary key auto_increment,
     customer_name varchar(100) not null,
-    email varchar(50) unique,
+    email varchar(50) not null unique,
     phone varchar(15) unique,
-    address varchar(100) not null
+    address varchar(100)
 );
 
 create table orders (
@@ -34,16 +34,19 @@ create table order_detail (
     quantity int not null,
     price_at_time decimal(10, 2)
 );
-
+-- Thêm cột category (kiểu chuỗi - ví dụ: 'Keyboard', 'Switch', 'Keycap') vào bảng Product
 alter table product
 add category varchar(100);
 
+-- Đổi tên cột Hãng sản xuất trong bảng Product thành tên khác cùng nghĩa.
 alter table product
 rename column manufacturer to factory;
 
+-- Viết câu lệnh SQL để xóa bảng Order_Detail và bảng Orders
 delete from order_detail;
 delete from orders;
 
+-- Thêm ít nhất 5 bản ghi hợp lệ cho mỗi bảng. Dữ liệu cần có sự liên kết logic thông qua các khóa ngoại
 insert into customer (customer_name, email, phone, address)
 values
 ('Nguyễn A', 'a@gmail.com', '01478523690', 'HCM'),
@@ -58,7 +61,8 @@ values
 (800000, 1),
 (800000, 2),
 (1800000, 3),
-(1900000, 5);
+(1900000, 5),
+(9000000, 5);
 
 insert into product (product_name, factory, price, stock, category)
 values
@@ -76,27 +80,30 @@ values
 (2, 2, 1, 800000),
 (3, 2, 1, 800000);
 
+-- Tăng giá bán thêm 10% cho tất cả các sản phẩm thuộc thương hiệu 'Aula'.
 set sql_safe_updates = 0;
 update product
 set price = price * 1.1
 where factory = 'Aula';
 
 -- Xóa thông tin những khách hàng chưa cung cấp địa chỉ.
-delete from customer
-where email is not null;
+delete  from customer
+where address is not null;
 
+--  Tìm tất cả các sản phẩm có đơn giá nằm trong khoảng từ 1.000.000 VNĐ đến 3.000.000 VNĐ.
 select * from product
 where price between 1000000 and 3000000;
 
 -- Liệt kê danh sách các đơn hàng được thực hiện trong tháng 04 năm 2026.
-select * from orders
-where month();
 
+-- Hiển thị danh sách các sản phẩm có tên chứa từ khóa 'Aula'.
 select * from product
-where factory = 'Aula';
+where product_name = 'Aula';
 
 -- Lấy ra mã khách hàng và tổng tiền của những đơn hàng có tổng giá trị lớn hơn 5.000.000 VNĐ.
+select customer_id, orders_total from orders
+where orders_total > 5000000 ;
 
-
+-- Hiển thị thông tin các sản phẩm có số lượng tồn kho dưới 10 sản phẩm để cửa hàng có kế hoạch nhập thêm.
 select * from product
 where stock < 10;
